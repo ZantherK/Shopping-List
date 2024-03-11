ce96ff72d00fac9e2a29b263d9a2f5cec7dc5e26
unknown <zantherk@outlook.com>
2024-03-11 12:09:49 -0600

    Button up

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +32,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>())}
+
                     Column(
                         modifier=Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.Center
@@ -34,9 +45,25 @@ class MainActivity : ComponentActivity() {
                         ){
                            Text(text = "Add Item")
                         }
+                        LazyColumn(
+                            modifier=Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ){
+                            items(sItems){
+
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+data class ShoppingItem(
+    val id:Int,
+    var name:String,
+    var quantity:Int,
+    var isEditing:Boolean = false
+                        )
