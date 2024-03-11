c6c1abbe6e9828aa3d3beee6ddfaace46bd7f31b
ZantherK <zantherk@outlook.com>
2024-03-11 13:45:17 -0600

    AlertDialog

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,22 +26,25 @@ data class ShoppingItem(
     var isEditing:Boolean = false
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
     var sItems by remember{ mutableStateOf(listOf<ShoppingItem>())}
+    var showDialog by remember { mutableStateOf(false)}
+    
 
     Column(
         modifier= Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ){
         Button(
-            onClick = {},
+            onClick = {showDialog=true},
             modifier=Modifier.align(Alignment.CenterHorizontally)
         ){
             Text(text = "Add Item")
         }
         LazyColumn(
-            modifier=Modifier
+            modifier= Modifier
                 .fillMaxSize()
                 .padding(16.dp)
         ){
@@ -48,4 +53,11 @@ fun ShoppingListApp(){
             }
         }
     }
+
+    if(showDialog){
+        AlertDialog(onDismissRequest = { showDialog = false}) {
+            Text(text = "alert dialog")
+        }
+    }
+    
 }
